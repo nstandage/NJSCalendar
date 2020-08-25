cdeb3c3639d2651ccdb07bf0138cdc04a5a2f43e
Nathan Standage <developer@nathanstandage.com>
2020-08-25 16:05:54 -0600

    Initial Commit

---
+++ NJSCalendar/NJSCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NJSCalendarApp.swift
+//  NJSCalendar
+//
+//  Created by Nathan on 8/25/20.
+//
+
+import SwiftUI
+
+@main
+struct NJSCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
